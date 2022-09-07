8aef4be7de98b0c5036741b96398cba63f943d11
Kevin Daysal <kdaysal123@gmail.com>
2022-09-06 22:12:38 -0400

    Initial commit of new grocery-list app project files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="container">
+      <h1>Hello From React</h1>
+      <h2>This is a change I am about to commit</h2>
+    </div>
+  );
+}
+
+export default App;

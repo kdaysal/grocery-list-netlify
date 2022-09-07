5c9f107f1aef3507613db1e84f715f1a06557132
Kevin Daysal <kdaysal123@gmail.com>
2022-09-06 22:14:42 -0400

    Testing new wip-1 branch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
     <div className="container">
       <h1>Hello From React</h1>
       <h2>This is a change I am about to commit</h2>
+      <h3>This is another change I am about to commit</h3>
     </div>
   );
 }

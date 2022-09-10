b1025ca8564d8a9a1db03565b7e1b91a7a4029d6
Kevin Daysal <kdaysal123@gmail.com>
2022-09-10 14:56:53 -0400

    Added 2 buttons for demonstration purposes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types'//prop-types is totally optional for a small app like this - I'm adding it here for reference
-
+import Button from './Button'
 
 const Header = ({ title }) => {
   return (
     <header>
       <h1 style={{ color: 'red', backgroundColor: 'black' }}>{title}</h1>
+      <Button btnText="Add" btnColor='green' />
+      <Button btnText="Delete" btnColor='red' />
     </header>
   )
 }
@@ -13,12 +15,12 @@ Header.propTypes = {
   title: PropTypes.string
 }
 
-//For sake of example - you can define styles like this...
+//For sake of example - you can also define styles separately at the bottom like this...
 // const headingStyle = {
 //   color: 'red',
 //   backgroundColor: 'black'
 // }
-//and if I wanted to call them, the syntax would be like this: <h1 style={headingStyle}>{title}</h1>
+//and if I wanted to call them, the syntax would be like this for example: <h1 style={headingStyle}>{title}</h1>
 
 
 export default Header

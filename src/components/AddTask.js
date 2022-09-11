541da313ef489e272d8f7814cc1c9f10d5785df7
Kevin Daysal <kdaysal123@gmail.com>
2022-09-10 21:22:31 -0400

    Implemented useState to update state of 'text', 'aisle', and 'reminder' when user adds a new task using the 'add-form' form.

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,17 +1,34 @@
+import { useState } from "react";
+
 const AddTask = () => {
+  const [text, setText] = useState('');
+  const [aisle, setAisle] = useState('');
+  const [reminder, setReminder] = useState(false);
+
   return (
     <form className='add-form'>
       <div className='form-control'>
         <label>Task</label>
-        <input type='text' placeholder='Add Task' />
+        <input type='text'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder='Add Task'
+        />
       </div>
       <div className='form-control'>
         <label>Aisle</label>
-        <input type='text' placeholder='Add Aisle' />
+        <input type='text'
+          placeholder='Add Aisle'
+          value={aisle}
+          onChange={(e) => setAisle(e.target.value)}
+        />
       </div>
       <div className='form-control form-control-check'>
         <label>Set Reminder</label>
-        <input type='checkbox' />
+        <input type='checkbox'
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
       </div>
       <input className='btn btn-block' type='submit' value='Save Task' />
     </form>

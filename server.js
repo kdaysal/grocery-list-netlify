44fc2c90e3fd1a8e704dcc7a9269c43624693cc9
Kevin Daysal <kdaysal123@gmail.com>
2022-10-22 16:13:06 -0400

    replaced database path in mongoose.connect statement with environment variable stored in .env file

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+require('dotenv').config(); //load all environment variables from the .env file
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
 app.listen(3000, () => console.log(`server is running`));
 
-mongoose.connect('mongodb://localhost/grocery-list', { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }); //see '.env' file for corresponding DATABASE_URL
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('connected to database, yay!'));
+db.on('error', (error) => console.error(error)); //if there is an error connecting to my db, log the error
+db.once('open', () => console.log('connected to database, yay!')); //if I successfully connect, log this message (will only run once)
 

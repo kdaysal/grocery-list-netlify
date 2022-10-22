7ac8152a195d36a95327222901864d05c803e004
Kevin Daysal <kdaysal123@gmail.com>
2022-10-22 16:04:27 -0400

    Can successfully start mongosh (the new mongo shell) and connec to my db
    -db name = "grocery-list"

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.listen(3000, () => console.log(`server is running`));
+
+mongoose.connect('mongodb://localhost/grocery-list', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('connected to database, yay!'));
+

6eb9adccd061df0672be96a996d9aa44aec62591
Kevin Daysal <kdaysal123@gmail.com>
2022-10-23 16:11:40 -0400

    Updated data types for my User model

--- models/user.js
+++ models/user.js
@@ -9,8 +9,11 @@ const userSchema = new mongoose.Schema({
     required: true
   },
   groceryListItems: {
-    itemName: "pizza",
+    itemName: String,
     aisle: String, // note - aisle could be a number OR a department, such as "deli", so I will make it into a String, and any actual numbers can be passed as strings like "3"
-    reminder: false
+    reminder: Boolean
   }
 })
+
+// the '.model()' function will allow me to interact directly with my dabaste USING the schema I defined in this file
+module.exports = mongoose.model('User', userSchema)

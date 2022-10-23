5b97b03486de63fb4c187799c49bfb42fe42324b
Kevin Daysal <kdaysal123@gmail.com>
2022-10-23 15:25:35 -0400

    I assigned the port to a variable to be either process.env.PORT or 3000

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config(); //load all environment variables from the .env file
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }); //see '.env' file for corresponding DATABASE_URL
 const db = mongoose.connection;
@@ -13,7 +14,7 @@ db.once('open', () => console.log('connected to database, yay!')); //if I succes
 
 app.use(express.json()) //this allows the server to accept JSON as a BODY (instead of a POST element or a GET element, etc)
 //Setting up the routes
-const usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users'); //since all routes will stem from the root url + '/users', this lets me shorthand it so in my routes (in users.js) I don't have to specify '/users' for each one.
 app.use('/users', usersRouter);
 
-app.listen(3000, () => console.log(`server is running`));
+app.listen(port, () => console.log(`server is running on port: ${port}`));

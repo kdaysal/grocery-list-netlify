cc3480a2ca6ebec76375bca0e5f5b163f7219dfb
Kevin Daysal <kdaysal123@gmail.com>
2022-10-23 12:05:25 -0400

    Started building out my routes - created new "users.js" file

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+/* global require, module */ //this prevents eslint from marking these undefined vars as errors
+
+const express = require('express');
+const router = express.Router();
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+/* global require, process */ //this prevents eslint from marking these undefined vars as errors
+
 require('dotenv').config(); //load all environment variables from the .env file
 
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-app.listen(3000, () => console.log(`server is running`));
-
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }); //see '.env' file for corresponding DATABASE_URL
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error)); //if there is an error connecting to my db, log the error
 db.once('open', () => console.log('connected to database, yay!')); //if I successfully connect, log this message (will only run once)
 
+app.use(express.json()) //this allows the server to accept JSON as a BODY (instead of a POST element or a GET element, etc)
+//Setting up the routes
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
+
+app.listen(3000, () => console.log(`server is running`));

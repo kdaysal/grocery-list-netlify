2e28e03af326314094f3d06ad32f562f539e93ac
Kevin Daysal <kdaysal123@gmail.com>
2022-10-23 17:08:24 -0400

    Fixed syntax in my User schema to define groceryListItems as an array of objects.

--- models/user.js
+++ models/user.js
@@ -8,11 +8,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  groceryListItems: {
+  groceryListItems: [{
     itemName: String,
     aisle: String, // note - aisle could be a number OR a department, such as "deli", so I will make it into a String, and any actual numbers can be passed as strings like "3"
     reminder: Boolean
-  }
+  }]
 })
 
 // the '.model()' function will allow me to interact directly with my dabaste USING the schema I defined in this file

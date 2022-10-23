bc5b61b3f58546229f42288f245a8ccf0ca04697
Kevin Daysal <kdaysal123@gmail.com>
2022-10-23 15:34:51 -0400

    Simple test of .../users/:id route to confirm it is working.

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,15 @@ const express = require('express');
 const router = express.Router();
 
 // Get all users
-// to test with Postman
+// to test with Postman, run a GET call on url: http://localhost:3000/users
 router.get('/', (req, res) => {
-  res.send('Hello World from my / route')
+  res.send('Hello World from my /users route') // here the '/' is actually my '/users' route due to: <app.use('/users', usersRouter)> in my server.js file;
+
 })
 
 // Get one user
 router.get('/:id', (req, res) => {
-
+  res.send(`fetching details for user id: ${req.params.id}`);
 })
 
 // Create one user

711c897953414e75270862442005bca52104c90d
Kevin Daysal <kdaysal123@gmail.com>
2022-10-23 15:53:57 -0400

    Added the user's name to the title and centered the text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types'//prop-types is totally optional for a small app like this - I'm adding it here for reference
 import Button from '../Button/Button'
 
-const Header = ({ title, onAdd, showAddTask }) => {
+const Header = ({ title, onAdd, showAddTask, user }) => {
 
   return (
     <header>
-      <h1 style={{ color: 'red', backgroundColor: 'black' }}>{title}</h1>
+      <h1 style={headingStyle}>{title} - {user}</h1>
       <Button
         btnText={showAddTask ? 'Close' : 'Add'}
         btnColor={showAddTask ? 'blue' : 'green'}
@@ -19,12 +19,11 @@ Header.propTypes = {
   title: PropTypes.string
 }
 
-//For sake of example - you can also define styles separately at the bottom like this...
-// const headingStyle = {
-//   color: 'red',
-//   backgroundColor: 'black'
-// }
-//and if I wanted to call them, the syntax would be like this for example: <h1 style={headingStyle}>{title}</h1>
+const headingStyle = {
+  color: 'red',
+  backgroundColor: 'black',
+  textAlign: 'center'
 
+}
 
 export default Header

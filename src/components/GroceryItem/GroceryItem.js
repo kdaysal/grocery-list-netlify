485486764bbc40b2b9095cf46ecd152598c1970e
Kevin Daysal <kdaysal123@gmail.com>
2022-11-06 00:28:02 -0400

    Added BsToggle icons to each grocery item (just the bootstrap icon shell, no code yet).

--- src/components/GroceryItem/GroceryItem.js
+++ src/components/GroceryItem/GroceryItem.js
@@ -1,16 +1,22 @@
 //Component to house all details relating to an individual grocery item
 
-import { FaTimes } from 'react-icons/fa' //'font-awesome' icon to use for the 'X' (delete) button
-
+import { FaTimes } from 'react-icons/fa'; //'font-awesome' icon to use for the 'X' (delete) button
+import { BsToggleOff, BsToggleOn } from 'react-icons/bs';
 const GroceryItem = ({ groceryItem, onDelete, onToggle }) => {
 
   console.log(`groceryItem is: ${JSON.stringify(groceryItem)}`);
   return (
-    <div className={`groceryItem ${groceryItem.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(groceryItem.id)}>
-      <h3>{groceryItem.itemName} <FaTimes style={{ color: 'red', cursor: 'pointer' }}
-        onClick={() => onDelete(groceryItem._id)}
-      />
+    <div className={`groceryItem ${groceryItem.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(groceryItem._id)}>
+      <h3>{groceryItem.itemName}
+        {!groceryItem.reminder && <BsToggleOff />
+        }
+        {groceryItem.reminder && <BsToggleOn />
+        }
+        <FaTimes style={{ color: 'red', cursor: 'pointer' }}
+          onClick={() => onDelete(groceryItem._id)}
+        />
       </h3>
+
       <p>Aisle/Dept: {groceryItem.aisle}</p>
     </div>
   )

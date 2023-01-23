6f2898e541df6df9a630f2d3451c794f62d04c58
Kevin Daysal <kdaysal123@gmail.com>
2023-01-22 21:29:14 -0500

    Added styling to center-align buttons and fix spacing between EditUser icon and username.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'//prop-types is totally optional for a small app like this - I'm adding it here for reference
-import Button from '../Button/Button'
+import Button from '../Button/Button';
 import { AiOutlineEdit } from 'react-icons/ai';
+import './Header.css';
 
 const Header = ({
   title,
@@ -25,92 +26,101 @@ const Header = ({
   return (
     <header>
       <h1 style={headingStyle}>{title} {userName ?
-        '- ' + userName
-        : ''} &nbsp; &nbsp;
-        {userName && <AiOutlineEdit
-          style={{ color: 'white' }}
-          onClick={() => {
-            onEditShow();
-          }}
-        />}
+        ': ' + userName
+        : ''}
+        {userName &&
+          <AiOutlineEdit
+            className="edit-user-icon"
+            style={{ color: 'white' }}
+            onClick={() => {
+              onEditShow();
+            }}
+          />}
       </h1>
       {/* If no current 'user' is set, present buttons with all possible user names for the person to choose from (i.e. to set a single 'user')*/}
       {!userName && (
         allUserData.map((user, index) =>
-          <Button
-            key={index}
-            btnText={user.name}
-            btnColor={'orange'}
-            onClick={() => updateUserSession(user.name, user)}
-          >
-          </Button>)
+          <div className='user-button-wrapper'>
+            <Button
+              className="user-button"
+              key={index}
+              btnText={user.name}
+              btnColor={'orange'}
+              onClick={() => updateUserSession(user.name, user)}
+            >
+            </Button>
+          </div>
+        )
       )
       }
 
       {/* If (user) then show a 'Change User button' */}
-      {userName && (
-        <Button
-          btnText={'Change User'}
-          btnColor={'blue'}
-          onClick={() => updateUserSession('', [])}
-        />
-      )
-      }
+      <div class="header-button">
+        {userName && (
+          <Button
+            btnText={'Change User'}
+            btnColor={'blue'}
+            onClick={() => updateUserSession('', [])}
+          />
+        )
+        }
 
-      {/* Only show the Add button if no current 'user' is set */}
+        {/* Only show the Add button if no current 'user' is set */}
+        {userName && (
+          <Button
+            btnText={showAddTask ? 'Close' : 'Add Item'}
+            btnColor={showAddTask ? 'blue' : 'green'}
+            onClick={onAdd}
+          />
+        )
+        }
+      </div>
       {userName && (
-        <Button
-          btnText={showAddTask ? 'Close' : 'Add Item'}
-          btnColor={showAddTask ? 'blue' : 'green'}
-          onClick={onAdd}
-        />
-      )
-      }
-      {userName && (
-        <h3>Now viewing: {(visibilityFilter == 'all' ? 'All items' : (visibilityFilter == 'reminder-only' ? 'Items I need' : (visibilityFilter == 'no-reminder' ? 'Items I have' : null)))}</h3>
+        <h3 id='now-viewing'>Now viewing: {(visibilityFilter == 'all' ? 'All items' : (visibilityFilter == 'reminder-only' ? 'Items I need' : (visibilityFilter == 'no-reminder' ? 'Items I have' : null)))}</h3>
       )
       }
       {/* Display 'visibility filter' buttons to let the user choose whether they want to see only 'reminder' items, only '!reminder' items, or 'all' items */}
-      {((userName) && (visibilityFilter != 'all')) && (
-        <Button
-          btnText={'Show All Items'}
-          btnColor={'orange'}
-          onClick={() => {
-            console.log(`All Items clicked`)
-            updateVisibilityFilter('all');
-          }
-          }
-        />
-      )
-      }
-      {/* Only see 'Items I need' (reminder-only) */}
-      {((userName) && (visibilityFilter != 'reminder-only')) && (
-        <Button
-          btnText={'Show Items I need'}
-          btnColor={'orange'}
-          onClick={() => {
-            console.log(`Items I need clicked`)
-            updateVisibilityFilter('reminder-only');
-          }
-          }
-        />
-      )
-      }
+      <div class="show-items-button">
+        {((userName) && (visibilityFilter != 'all')) && (
+          <Button
+            btnText={'Show All Items'}
+            btnColor={'orange'}
+            onClick={() => {
+              console.log(`All Items clicked`)
+              updateVisibilityFilter('all');
+            }
+            }
+          />
+        )
+        }
+        {/* Only see 'Items I need' (reminder-only) */}
+        {((userName) && (visibilityFilter != 'reminder-only')) && (
+          <Button
+            btnText={'Show Items I need'}
+            btnColor={'orange'}
+            onClick={() => {
+              console.log(`Items I need clicked`)
+              updateVisibilityFilter('reminder-only');
+            }
+            }
+          />
+        )
+        }
 
-      {/* Only see 'Items I have' (no-reminder) */}
-      {((userName) && (visibilityFilter != 'no-reminder')) && (
-        <Button
-          btnText={'Show Items I have'}
-          btnColor={'orange'}
-          onClick={() => {
-            console.log(`Items I have clicked`)
-            updateVisibilityFilter('no-reminder');
-          }
-          }
-        />
-      )
-      }
-
+        {/* Only see 'Items I have' (no-reminder) */}
+        {((userName) && (visibilityFilter != 'no-reminder')) && (
+          <Button
+            btnText={'Show Items I have'}
+            btnColor={'orange'}
+            onClick={() => {
+              console.log(`Items I have clicked`)
+              updateVisibilityFilter('no-reminder');
+            }
+            }
+          />
+        )
+        }
+      </div>
     </header>
   )
 }
